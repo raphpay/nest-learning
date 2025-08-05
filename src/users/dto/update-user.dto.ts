4e87c4946f0d4e643831a58c69cce80057a3347a
raphpay <raphpay.dev@gmail.com>
2025-08-05 08:43:24 +0400

    Validate data #4

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,7 +1,11 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { IsEmail, IsString } from 'class-validator';
 import { CreateUserDto } from './create-user.dto';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @IsString()
   name?: string;
-  age?: number;
+
+  @IsEmail()
+  email?: string;
 }
